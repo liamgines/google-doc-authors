d052cf57814af7e5d8882c6b0d7703c2e0a07b64
liamgines <109954426+liamgines@users.noreply.github.com>
2026-04-13 01:04:36 -0700

    Create table using query stored in file

--- server/database/seed.ts
+++ server/database/seed.ts
@@ -1,11 +1,22 @@
 const pool = require("./pool.ts");
+const path = require("node:path");
+const fs = require("node:fs");
+
+async function queryDatabaseWithFile(pool, filePath) {
+    const readFileOptions = { encoding: "utf-8", flag: "r" };
+    const sql = fs.readFileSync(filePath, readFileOptions);
+    return await pool.query(sql);
+}
 
 async function seedDatabase() {
-    await pool.query(`DROP TABLE IF EXISTS users`);
-    await pool.query(`CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT)`);
-    await pool.query(`INSERT INTO users (name) VALUES ($1), ($2)`, ["one", "two"]);
-    const result = await pool.query(`SELECT * FROM users`);
+    await queryDatabaseWithFile(pool, path.join(__dirname, "./reset.sql"));
+    await queryDatabaseWithFile(pool, path.join(__dirname,  "./seed.sql"));
+
+    /*
+    await pool.query(`INSERT INTO authors DEFAULT VALUES`);
+    const result = await pool.query(`SELECT * FROM authors`);
     console.log(result.rows);
+    */
 
     await pool.end();
 };
