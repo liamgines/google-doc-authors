73325ae1f4cc585a41cc34b7a009579364f8af9a
liamgines <109954426+liamgines@users.noreply.github.com>
2026-04-18 02:10:54 -0700

    Add functions to create and select users with a google account id

---
+++ server/database/usersTable.ts
@@ -0,0 +1,24 @@
+import pool from "./pool";
+import { databaseQueryOnlyRow } from "./helpers";
+
+export async function getUserByGoogleAccountId(googleAccountId: string) {
+    return await databaseQueryOnlyRow(pool, `SELECT * FROM users WHERE (google_account_id = $1);`, [googleAccountId]);
+}
+
+export async function createUser(googleAccountId: string): Promise<any> {
+    try {
+        // https://stackoverflow.com/q/34966841/32242805
+        return await pool.query(`INSERT INTO users (google_account_id)
+                                 VALUES ($1) RETURNING *;`, [googleAccountId]);
+    }
+    catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+export async function createUserIfNotExists(googleAccountId: string): Promise<any> {
+    let user = await getUserByGoogleAccountId(googleAccountId);
+    if (!user) user = await createUser(googleAccountId);
+    return user;
+}
