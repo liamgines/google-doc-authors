4307179775481233d5f4b628a2ecf71eaeea055a
liamgines <109954426+liamgines@users.noreply.github.com>
2026-04-18 01:11:38 -0700

    Rename authors to users and introduce google account id field to table

--- server/database/seed.sql
+++ server/database/seed.sql
@@ -1,3 +1,5 @@
-CREATE TABLE IF NOT EXISTS authors (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    google_account_id VARCHAR(255) NOT NULL,  -- "Maximum length of 255 case-sensitive ASCII characters" (https://developers.google.com/identity/openid-connect/openid-connect).
+    CONSTRAINT uk_users_google_account_id UNIQUE (google_account_id)
 );
