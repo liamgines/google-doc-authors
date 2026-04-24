35bfa1fb2a77708b35949f1e3c23231ec6ce8dae
liamgines <109954426+liamgines@users.noreply.github.com>
2026-04-23 19:46:22 -0700

    Display text spacing properly, test assigning colors by user id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,15 +10,25 @@ function QuotesDisplay({ revisions }) {
     const quotes = revisions.quotes;
     const permissionIdUsers = revisions.permissionIdUsers;
 
-    const quoteParagraphs = quotes.map(quote => {
+    let i = 0;
+    let permissionIdColors: any = {};
+    for (let permissionId in permissionIdUsers) {
+        permissionIdColors[permissionId] = (i % 2) ? "red" : "green";
+        i++;
+    }
+
+    i = 0;
+    const quoteSpans = quotes.map(quote => {
         let permissionId = quote.permissionId;
         let user = permissionIdUsers[permissionId];
         let userName = user.displayName;
         let userEmail = user.emailAddress;
 
-        return (<p>{userName} ({userEmail}): {quote.text}</p>);
+        let userColor = permissionIdColors[permissionId];
+        // For displaying whitespace properly: https://stackoverflow.com/a/69436906/32242805
+        return (<span key={i++} id={permissionId} style={{ backgroundColor: userColor, whiteSpace: "pre-line" }}>{quote.text}</span>);
     });
-    return (<><div className="quotes">{quoteParagraphs}</div></>);
+    return (<p id="quotes">{quoteSpans}</p>);
 }
 
 function App() {
