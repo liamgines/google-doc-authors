1572870fdc24efbc01db387ca709e789c30b0fc3
liamgines <109954426+liamgines@users.noreply.github.com>
2026-04-26 21:48:43 -0700

    Assign user colors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,25 @@ import GoogleDocsPicker from "./GoogleDocsPicker";
 
 const clientUser = await (await fetch("/api/authorize/user-with-google-drive-access")).json();
 
+// https://sashamaps.net/docs/resources/20-colors/
+const HEX_COLORS = ["#E6194B", "#3CB44B", "#FFE119", "#4363D8", "#F58231", "#911EB4", "#42D4F4", "#F032E6", "#BFEF45", "#FABED4", "#469990", "#DCBEFF", "#9A6324", "#FFFAC8", "#800000", "#AAFFC3", "#808000", "#FFD8B1", "#000075"];
+const ANONYMOUS_HEX_COLOR = "#A9A9A9";
+
+// https://stackoverflow.com/a/47201559/32242805
+function hexToRGB(hex: string, alpha: string) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha) return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const USER_COLOR_OPACITY = "0.25";
+const RGBA_COLORS = HEX_COLORS.map(hex => hexToRGB(hex, USER_COLOR_OPACITY));
+const ANONYMOUS_PERMISSION_ID = "";
+const ANONYMOUS_RGBA_COLOR = hexToRGB(ANONYMOUS_HEX_COLOR, USER_COLOR_OPACITY);
+
 function QuotesDisplay({ user, revisions }) {
     if (!user || !revisions) return (<></>);
 
@@ -12,8 +31,14 @@ function QuotesDisplay({ user, revisions }) {
 
     let i = 0;
     let permissionIdColors: any = {};
+    permissionIdColors[ANONYMOUS_PERMISSION_ID] = ANONYMOUS_RGBA_COLOR;
+
+    const numColors = RGBA_COLORS.length;
     for (let permissionId in permissionIdUsers) {
-        permissionIdColors[permissionId] = (i % 2) ? "red" : "green";
+        if (permissionId === ANONYMOUS_PERMISSION_ID) continue;
+
+        // https://stackoverflow.com/a/5135033/32242805
+        permissionIdColors[permissionId] = RGBA_COLORS[i % numColors];
         i++;
     }
 
