dee05199a338f62f5446a9f31065a04f654b78c3
liamgines <109954426+liamgines@users.noreply.github.com>
2026-05-01 16:50:16 -0700

    Add doc and user doc tables to database

--- server/database/reset.sql
+++ server/database/reset.sql
@@ -1,2 +1,4 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS docs CASCADE;
+DROP TABLE IF EXISTS userdocs CASCADE;
 DROP TABLE IF EXISTS session;   -- From node_modules/connect-pg-simple/table.sql
--- server/database/seed.sql
+++ server/database/seed.sql
@@ -3,3 +3,19 @@ CREATE TABLE IF NOT EXISTS users (
     google_account_id VARCHAR(255) NOT NULL,  -- "Maximum length of 255 case-sensitive ASCII characters" (https://developers.google.com/identity/openid-connect/openid-connect).
     CONSTRAINT uk_users_google_account_id UNIQUE (google_account_id)
 );
+
+CREATE TABLE IF NOT EXISTS docs (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    google_id TEXT NOT NULL,
+    CONSTRAINT uk_docs_google_id UNIQUE (google_id)
+);
+
+CREATE TABLE IF NOT EXISTS userdocs (
+    user_id INTEGER,
+    doc_id INTEGER,
+    revision_id TEXT NOT NULL,
+    path TEXT,
+    PRIMARY KEY (user_id, doc_id),
+    CONSTRAINT fk_userdocs_users FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_userdocs_docs FOREIGN KEY (doc_id) REFERENCES docs (id)
+);
