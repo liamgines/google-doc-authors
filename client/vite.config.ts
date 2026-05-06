b1343f87ac31a7b54b4255ae26c4d6fd468c055c
liamgines <109954426+liamgines@users.noreply.github.com>
2026-05-05 17:06:21 -0700

    Move .env info to one file in the root directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
+import path from "node:path";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const envDirectory = path.join(__dirname, "../");
+  const env = loadEnv(mode, envDirectory);
   return {
+      envDir: envDirectory,
       plugins: [react()],
       server: { proxy: { "/api": `http://localhost:${env.VITE_API_PORT}` } }
   }
