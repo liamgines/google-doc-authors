ccb6e874c16412056c7ed85dcad9a188d7034a08
liamgines <109954426+liamgines@users.noreply.github.com>
2026-05-05 17:51:04 -0700

    Ensure .env loads properly when setting up proxy on client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  const envPrefix = "PUBLIC_"
   const envDirectory = path.join(__dirname, "../");
-  const env = loadEnv(mode, envDirectory);
+  const env = loadEnv(mode, envDirectory, envPrefix);
   return {
       envDir: envDirectory,
-      envPrefix: "PUBLIC_",
+      envPrefix: envPrefix,
       plugins: [react()],
       server: { proxy: { "/api": `http://localhost:${env.PUBLIC_SERVER_PORT}` } }
   }
