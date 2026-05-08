fad242272cf1f321bc5bf1208ff1ed3e066e77a5
liamgines <109954426+liamgines@users.noreply.github.com>
2026-05-07 18:59:26 -0700

    Add page to display author contributions across all user-submitted docs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route, Outlet, Navigate, Link } from "react-router";
 import GoogleAuthorization from "./GoogleAuthorization";
 import GoogleLogout from "./GoogleLogout";
 import GoogleDocsList from "./GoogleDocsList";
 import GoogleDocQuotes from "./GoogleDocQuotes";
+import Authors from "./Authors";
 
 const clientUser = await (await fetch("/api/authorize/user-with-google-drive-access")).json();
 
@@ -14,7 +15,11 @@ function VisitorOnlyRoute({ user }) {
 }
 
 function UserOnlyRoute({ user, setUser }) {
-    if (user) return (<><GoogleLogout setUser={setUser} /><Outlet /></>);
+    if (user) return (<>
+    <Link to="/authors">Authors</Link>
+    <GoogleLogout setUser={setUser} />
+    <Outlet />
+    </>);
     return (<Navigate to="/login" replace />);
 }
 
@@ -31,6 +36,7 @@ function App() {
            <Route element={<UserOnlyRoute user={user} setUser={setUser} />}>
                 <Route index element={<GoogleDocsList user={user} setUser={setUser} />} />
                 <Route path="/docId/:id" element={<GoogleDocQuotes />} />
+                <Route path="/authors" element={<Authors />} />
            </Route>
         </Routes>
         </BrowserRouter>
---
+++ client/src/Authors.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function AuthorsList(permissionIdAuthors) {
+    let paragraphs = [];
+    for (const permissionId in permissionIdAuthors) {
+        const author = permissionIdAuthors[permissionId];
+        paragraphs.push(<p>{author.displayName} {author.emailAddress ? `(${author.emailAddress})` : ""} | {author.totalChars} characters</p>);
+    }
+    return paragraphs;
+}
+
+export default function Authors() {
+    const [permissionIdAuthors, setPermissionIdAuthors] = useState(null);
+
+    async function fetchPermissionIdAuthors() {
+        try {
+            const response = await fetch("/api/authors");
+            if (!response.ok) return console.error("Error: Failed to fetch authors");
+
+            const permissionIdAuthors = await response.json();
+            setPermissionIdAuthors(permissionIdAuthors);
+        }
+        catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchPermissionIdAuthors();
+    }, []);
+
+    if (!permissionIdAuthors) return (<p>Retrieving authors...</p>);
+    return (<>
+        {AuthorsList(permissionIdAuthors)}
+    </>);
+}
