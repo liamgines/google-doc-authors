a2b949672035356b653f88dcc29bfef748b3cc3d
liamgines <109954426+liamgines@users.noreply.github.com>
2026-05-09 03:58:07 -0700

    Add modified time in queries, add function to get user-submitted docs

--- server/database/userDocsTable.ts
+++ server/database/userDocsTable.ts
@@ -1,5 +1,5 @@
 import pool from "./pool";
-import { databaseQueryOnlyRow } from "./helpers";
+import { databaseQueryOnlyRow, databaseQueryRows } from "./helpers";
 import { getUserByGoogleAccountId } from "./usersTable";
 import { getDocByGoogleId } from "./docsTable";
 
@@ -15,9 +15,9 @@ export async function getUserDocByGoogleIds(userGoogleId: string, docGoogleId: s
 
 // https://blog.purestorage.com/purely-technical/sql-update-vs-insert-vs-upsert/
 // NOTE: A path that is an empty string indicates that the contributions are in the process of being determined
-export async function createUserDoc(userId: number, docId: number, revisionId: string, path: string = ""): Promise<any> {
+export async function createUserDoc(userId: number, docId: number, revisionId: string, modifiedTime: string, path: string = ""): Promise<any> {
     try {
-        return await databaseQueryOnlyRow(pool, `INSERT INTO userdocs (user_id, doc_id, revision_id, path) VALUES ($1, $2, $3, $4) RETURNING *;`, [userId, docId, revisionId, path]);
+        return await databaseQueryOnlyRow(pool, `INSERT INTO userdocs (user_id, doc_id, revision_id, modified_time, path) VALUES ($1, $2, $3, $4, $5) RETURNING *;`, [userId, docId, revisionId, modifiedTime, path]);
     }
     catch (error) {
         console.error(error);
@@ -26,19 +26,29 @@ export async function createUserDoc(userId: number, docId: number, revisionId: s
 }
 
 // NOTE: A path that is null indicates that the evaluation failed
-export async function updateRevisionIdAndPath(userId: number, docId: number, revisionId: string, path: string | null = null) {
-    return await databaseQueryOnlyRow(pool, "UPDATE userdocs SET revision_id = $1, path = $2 WHERE (user_id = $3 AND doc_id = $4) RETURNING *;", [revisionId, path, userId, docId]);
+export async function updateRevisionIdTimeAndPath(userId: number, docId: number, revisionId: string, modifiedTime: string, path: string | null = null) {
+    return await databaseQueryOnlyRow(pool, "UPDATE userdocs SET revision_id = $1, modified_time = $2, path = $3 WHERE (user_id = $4 AND doc_id = $5) RETURNING *;", [revisionId, modifiedTime, path, userId, docId]);
 }
 
 // NOTE: A path that is not null and not empty indicates that the contribution evaluation succeeded
-export async function createOrUpdateUserDoc(userGoogleId: string, docGoogleId: string, revisionId: string, path: string | null = ""): Promise<any> {
+export async function createOrUpdateUserDoc(userGoogleId: string, docGoogleId: string, revisionId: string, modifiedTime: string, path: string | null = ""): Promise<any> {
     const user = await getUserByGoogleAccountId(userGoogleId);
     const doc = await getDocByGoogleId(docGoogleId);
 
     let userdoc = await getUserDocByIds(user.id, doc.id);
     // The typescript error reported below this line should not be a problem because if there is no user doc, we can assume that the doc will start out being analyzed (meaning the path should never be null to begin with)
-    if (!userdoc) userdoc = await createUserDoc(user.id, doc.id, revisionId, path);
-    else          userdoc = await updateRevisionIdAndPath(user.id, doc.id, revisionId, path);
+    if (!userdoc) userdoc = await createUserDoc(user.id, doc.id, revisionId, modifiedTime, path);
+    else          userdoc = await updateRevisionIdTimeAndPath(user.id, doc.id, revisionId, modifiedTime, path);
 
     return userdoc;
+}
+
+/*
+export async function getClientUserDocByIds(userId: number, docId: number): Promise<any> {
+    return await databaseQueryOnlyRow(pool, `SELECT docs.id, docs.google_id, docs.name, userdocs.modified_time FROM docs INNER JOIN userdocs ON docs.id = userdocs.doc_id AND userdocs.user_id = $1 AND docs.id = $2;`, [userId, docId]);
+}
+*/
+
+export async function getAllSubmittedByUser(userId: number): Promise<any> {
+    return await databaseQueryRows(pool, `SELECT docs.id, docs.google_id, docs.name, userdocs.modified_time FROM docs INNER JOIN userdocs ON docs.id = userdocs.doc_id AND userdocs.user_id = $1;`, [userId]);
 }
